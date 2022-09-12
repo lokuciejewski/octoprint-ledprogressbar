25222e713a034d476689d3877a8ca39f83a2aea5
lokuciejewskim <grododzierzca@gmail.com>
2022-09-12 23:32:31 +0200

    initial commit

---
+++ octoprint_ledprogressbar/__init__.py
@@ -0,0 +1,109 @@
+# coding=utf-8
+from __future__ import absolute_import
+import octoprint.plugin as plugin
+import logging
+import smbus2
+from octoprint.events import Events
+
+class Colour:
+    red: int
+    green: int
+    blue: int
+
+    def __init__(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+
+class LEDProgressBar(self):
+
+    def __init__(self):
+        self.bus = smbus2.SMBus(1)
+        self.device_address = 0x69
+
+    def set_progress(self, percentage: float, colour: Colour):
+        logging.info(
+            f"Sending percentage {percentage} with colour RGB({colour.red}, {colour.green}, {colour.blue})")
+        self.bus.write_i2c_block_data(self.device_address, 0, int(
+            percentage), colour.red, colour.green, colour.blue)
+
+
+class LEDProgressBarPlugin(plugin.StartupPlugin, octoprint.plugin.SettingsPlugin,
+                           octoprint.plugin.AssetPlugin,
+                           octoprint.plugin.TemplatePlugin):
+
+    def __init__(self, *args, **kwargs):
+        super(LEDProgressBar, self).__init__(*args, **kwargs)
+        self.p_bar = LEDProgressBar()
+
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+        # for details.
+        return {
+            "ledprogressbar": {
+                "displayName": "Ledprogressbar Plugin",
+                "displayVersion": self._plugin_version,
+
+                # version check: github repository
+                "type": "github_release",
+                "user": "grododzierzca",
+                "repo": "OctoPrint-LEDProgressBar",
+                "current": self._plugin_version,
+
+                # update method: pip
+                "pip": "https://github.com/grododzierzca/OctoPrint-LEDProgressBar/archive/{target_version}.zip",
+            }
+        }
+    
+     # ~~ SettingsPlugin mixin
+
+    def get_settings_defaults(self):
+        return {
+            # put your plugin's default settings here
+        }
+
+    # ~~ AssetPlugin mixin
+
+    def get_assets(self):
+        # Define your plugin's asset files to automatically include in the
+        # core UI here.
+        return {
+            "js": ["js/ledprogressbar.js"],
+            "css": ["css/ledprogressbar.css"],
+            "less": ["less/ledprogressbar.less"]
+        }
+
+    # ~~ Softwareupdate hook
+
+    def on_after_startup(self):
+        self._logger.info("LED Progress Bar loaded!")
+
+    def on_event(self, event, payload):
+        if event == Events.PRINT_STARTED:
+            self.p_bar.set_progress(0, Colour(0, 10, 0))
+
+        elif event == Events.PRINT_DONE:
+            self.p_bar.set_progress(100, Colour(0, 10, 10)) 
+
+    def on_print_progress(self, storage, path, progress):
+        self.p_bar.set_progress(progress, Colour(0, 10, 0))
+
+
+
+
+__plugin_name__ = "LED ProgressBar"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "LED Progress Bar module for OctoPrint"
+__plugin_pythoncompat__ = ">=3.7,<4"
+
+
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = LEDProgressBarPlugin()
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
---
+++ octoprint_ledprogressbar/static/js/ledprogressbar.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-LEDProgressBar
+ *
+ * Author: You
+ * License: AGPLv3
+ */
+$(function() {
+    function LedprogressbarViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: LedprogressbarViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_ledprogressbar, #tab_plugin_ledprogressbar, ...
+        elements: [ /* ... */ ]
+    });
+});
